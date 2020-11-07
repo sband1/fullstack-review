a2822afe6c74c9fb8c615530f4f9b4d14b809eb9
sband1 <sbandeali1@gmail.com>
2020-11-07 10:22:16 -0800

    Updated front end to render only vaild details

--- client/src/components/RepoListItem.jsx
+++ client/src/components/RepoListItem.jsx
@@ -6,9 +6,9 @@ const RepoListItem = (props) => {
   return (
     <li>
       <span><a href={props.repo.repo_url}><text>{props.repo.repo_name}</text></a></span> <br></br>
-      <span style={{display: !props.repo.description && "none"}}>{props.repo.description}</span> <br></br>
-      <span>{props.repo.updated_at}</span> <br></br>
-      <span>{props.repo.forks_count}</span>
+      {props.repo.description? <span><span>Description: {props.repo.description}</span><br></br></span> : null}
+      <span>Date: {props.repo.updated_at}</span> <br></br>
+      <span>Forks: {props.repo.forks_count}</span>
     </li>
   )
 }
